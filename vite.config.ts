eaf53e9fd306b169b299ac2b0d36d7e620dce6bf
Issa <issa.djehon@gmail.com>
2026-06-04 11:41:08 +0200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? "/orange-bleue-annecy/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
     },
   },
-});
+}));
